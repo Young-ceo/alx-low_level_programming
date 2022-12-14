7aee28b0072619e2eec262f5b9e38c01739044f7
Young-ceo <kingsleymba01@gmail.com>
2022-12-13 19:44:53 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -17,7 +17,9 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r');						_putchar('\n');						
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 
 }
